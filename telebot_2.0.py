2102de794c2adb9b3a824970eedefa0055cfcccc
uri dar <uridar70@gmail.com>
2023-07-07 18:05:49 +0300

    update to the telebot script that work with mysql db inside the users class

--- telebot_2.0.py
+++ telebot_2.0.py
@@ -2,16 +2,12 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 import time
-import pickle
+import re
 import telebot
 from telebot import TeleBot
 from telebot import types
 import mysql.connector
 from mysql.connector import Error
-
-
-
-
 BOT_TOKEN = "6108637814:AAFSs4JhYcgAD0IQ42rm61ttHS9WQ4ib0F4"
 
 class User:
@@ -27,42 +23,12 @@ class User:
         self.station_to_get_update = station_to_get_update
         self.chat_ids_.append(chat_id)
         User.golesh_count += 1
-        conn = mysql.connector.connect(
-            user='root', password='Uri0524734764', host='127.0.0.1', database='windbot_db')
-        # open cursor, define and run query, fetch results
-        cursor = conn.cursor()
-        # Preparing SQL query to INSERT a record into the database.
-        sql = "INSERT INTO users(id, firs_name,stage, station, wind, wind_max )"\
-              "VALUES (%s, %s, %s, %s, %s, %s)"
-        staff = (chat_id, first_name, user_stage, str(station_to_get_update), wind_to_alert_knots, wind_to_alert_max)
-        try:
-           # Executing the SQL command
-            cursor.executem(sql, staff)
-            # Commit your changes in the database
-            conn.commit()
-            sql ="SELECT * FROM users"
-            cursor.execute(sql)
-            r= cursor.fetchall()
-            print(r)
-        except:
-            print('as')
-            sql = "UPDATE users " \
-                  "SET stage = %s ,station = %s, wind = %s, wind_max = %s " \
-                  " WHERE id = " + str(chat_id) + ""
-            staff = (user_stage, str(station_to_get_update), wind_to_alert_knots, wind_to_alert_max)
-            # Executing the SQL command
-            cursor.execute(sql, staff)
-            # Commit your changes in the database
-            conn.commit()
 
-        # close the cursor and database connection
-        cursor.close()
-        conn.close()
 
 list_of_station = ['כנרת בית צידה', 'כנרת מגדל', 'בת גלים כנסיה', 'כנרת כפר נחום', 'כנרת מצוף טבחה', 'שרונה', 'בצת', 'נמל חדרה', 'עתלית המבצר', 'עכו', 'שבי ציון', 'שדות ים']
 list_of_golshim = []
 
-def db_update(user_id):
+def db_update(chat_id, first_name, station_to_get_update, wind_to_alert_knots, wind_to_alert_max):
     # try to run the block of code
     try:
 
@@ -73,23 +39,53 @@ def db_update(user_id):
         # open cursor, define and run query, fetch results
         cursor = conn.cursor()
         # Preparing SQL query to INSERT a record into the database.
-        sql = "INSERT INTO users(id)" "VALUES ("+user_id+")"
+        sql = "INSERT INTO users_reg(id, first_name, station, wind, wind_max )" \
+              "VALUES (%s, %s, %s, %s, %s)"
+        staff = (chat_id, first_name, str(station_to_get_update), wind_to_alert_knots, wind_to_alert_max)
 
         try:
-            # Executing the SQL command
-            cursor.execute(sql)
-
-            # Commit your changes in the database
+                # Executing the SQL command
+            cursor.execute(sql, staff)
+                # Commit your changes in the database
             conn.commit()
-            # close the cursor and database connection
+                # close the cursor and database connection
             cursor.close()
-            conn.close()
+
         except:
-            print('the user id allready in the table')
+            print('a problem happend when we try to insert '+str(first_name)+ ' to the new_db')
 
     # catch exception and print error message
     except Error as err:
         print('Error message: ' + err.msg)
+
+def db_update_del(chat_id):
+    # try to run the block of code
+    try:
+        # establishing the connection
+        conn = mysql.connector.connect(
+            user='root', password='Uri0524734764', host='127.0.0.1', database='windbot_db')
+        # open cursor, define and run query, fetch results
+        cursor = conn.cursor()
+        # Preparing SQL query to INSERT a record into the database.
+        sql = "DELETE FROM users_reg " \
+              "WHERE id ='%s'"
+        staff = ([chat_id])
+
+        #try:
+            # Executing the SQL command
+        cursor.execute(sql, staff)
+            # Commit your changes in the database
+        conn.commit()
+             # close the cursor and database connection
+        cursor.close()
+
+        #except:
+         #   print('a problem happend when we try to delete '+str(chat_id)+ ' from the users_reg db')
+
+    # catch exception and print error message
+    except Error as err:
+        print('Error message: ' + err.msg)
+
 
 def db_get():
     # try to run the block of code
@@ -102,13 +98,13 @@ def db_get():
         # open cursor, define and run query, fetch results
         cursor = conn.cursor()
         # Preparing SQL query to INSERT a record into the database.
-        sql = "SELECT * FROM users"
+        sql = "SELECT id FROM users_reg"
            # Executing the SQL command
         cursor.execute(sql)
         result = cursor.fetchall()
         final_result = []
         for item in result:
-            item = list(item)
+            item = str(item)
             final_result.append(item)
         # close the cursor and database connection
         cursor.close()
@@ -124,14 +120,26 @@ users_dic = {}
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-file_name = 'users_data.pkl'
+# make a list of all the ids of users that allready register to the servise
+users_reg_id_list =[]
+for id in db_get():
+    string = id
+# Remove non-digit characters using regular expressions
+    chat_id = re.sub(r'\D', '', string)
+    users_reg_id_list.append(chat_id)
+    users_dic[int(chat_id)] = User(user_stage=5, chat_id=int(chat_id), first_name=300,
+                              wind_to_alert_knots=300, wind_to_alert_max=300, station_to_get_update=[])
+
+print(User.chat_ids_)
 @bot.message_handler(content_types=['text'])
 def welcome(message):
 
+
     chat_id = message.chat.id
     first_name = message.chat.first_name
-
-    if chat_id not in User.chat_ids_:
+    print(chat_id)
+    if chat_id not in users_dic.keys():
+        print('as')
         bot.send_message(chat_id, text='ברוכים הבאים לווינד בוט להרשמה לחץ כן')
         users_dic[chat_id] = User(user_stage=0, chat_id=chat_id, first_name=first_name,
                                             wind_to_alert_knots=0, wind_to_alert_max=0, station_to_get_update= [])
@@ -176,17 +184,16 @@ def welcome(message):
             user.station_to_get_update = list(stations)
             user.user_stage = 5
             bot.send_message(chat_id, text='תהליך ההרשמה הסתיים בהצלחה, מהיום תקבל עדכוני רוח!')
-            asd = User(user_stage=user.user_stage, chat_id= chat_id, first_name=first_name, wind_to_alert_knots=user.wind_to_alert_knots, wind_to_alert_max=user.wind_to_alert_max, station_to_get_update=user.station_to_get_update)
+            db_update(chat_id= chat_id, first_name=first_name, station_to_get_update=user.station_to_get_update, wind_to_alert_knots=user.wind_to_alert_knots, wind_to_alert_max=user.wind_to_alert_max)
         elif message.text in list_of_station:
             user.station_to_get_update.append(message.text)
 
     elif user.user_stage == 5:
         if message.text == '_delete_':
             del users_dic[chat_id]
+            db_update_del(chat_id)
         else:
             bot.send_message(chat_id, text='אתה כבר רשום לשירות. למחיקת המשתמש תשלח את ההודעה: _delete_')
 
-    for user_id, user in users_dic.items():
-        print(vars(user))
 
 bot.infinity_polling()
